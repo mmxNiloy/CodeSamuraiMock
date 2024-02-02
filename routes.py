01b9e9209d698378d357ef0d663d621e6a1c4e1c
Niloy Sarkar <dev.niloysarkar@gmail.com>
2024-02-02 17:48:56 +0600

    Put method done

--- routes.py
+++ routes.py
@@ -9,35 +9,68 @@ def index():
         'message': 'Hello from team Schadenfreude',
     }
 
-@app.route('/api/books', methods=['POST'])
+@app.route('/api/books', methods=['POST', 'GET'])
 def add():
-    data = request.get_json()
-    # jData = json.loads(data)
-    
-    id = data.get('id')
-    title = data.get('title')
-    author = data.get('author')
-    genre = data.get('genre')
-    price = data.get('price')
+    if request.method == 'POST':
+        data = request.get_json()
+        # jData = json.loads(data)
+        
+        id = data.get('id')
+        title = data.get('title')
+        author = data.get('author')
+        genre = data.get('genre')
+        price = data.get('price')
 
-    # print(id)
-    rec = Book(id=id, title=title, author=author, genre=genre, price=price)
-    # print(rec)
+        # print(id)
+        rec = Book(id=id, title=title, author=author, genre=genre, price=price)
+        # print(rec)
 
-    db.session.add(rec)
-    db.session.commit()
+        db.session.add(rec)
+        db.session.commit()
 
-    return Response(json.dumps(rec.to_dict()), status=201, mimetype='application/json')
+        return Response(json.dumps(rec.to_dict()), status=201, mimetype='application/json')
+    elif request.method == 'GET':
+        books = Book.query.order_by(Book.id).all()
+        print(books)
+        # Serialize data
+        serializedBooks = []
 
-@app.route('/api/books/<int:id>', methods=['GET'])
+        for book in books:
+            serializedBooks.append(book.to_dict())
+        
+        sData = {
+            'books': serializedBooks
+        }
+
+        return Response(json.dumps(sData), status=200, mimetype='application/json')
+
+@app.route('/api/books/<int:id>', methods=['GET', 'PUT'])
 def get_book(id):
     print(id)
-    
     b = Book.query.filter(Book.id == id).first()
-
-    print(b)
-
+    
     if b == None:
         return Response(json.dumps({'message': f'book with id: {id} was not found'}), status=404, content_type='application/json')
 
-    return Response(json.dumps(b.to_dict()), status=200, mimetype='application/json')
+    print(b)
+    
+    if request.method == 'GET':
+        return Response(json.dumps(b.to_dict()), status=200, mimetype='application/json')
+    if request.method == 'PUT':
+        # Change database
+        data = request.get_json()
+        
+        title = data.get('title')
+        author = data.get('author')
+        genre = data.get('genre')
+        price = data.get('price')
+
+        b.title = title
+        b.author = author
+        b.genre = genre
+        b.price = price
+
+        db.session.commit()
+
+        return Response(json.dumps(b.to_dict()), status=200, mimetype='application/json')
+
